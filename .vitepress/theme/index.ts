7b50c91443f96eaea7b2c0eae7b24bf7c0c8c6e5
TangbohuDianPaoZhang <93420604+TangbohuDianPaoZhang@users.noreply.github.com>
2025-03-16 16:00:30 +0800

    giscus config modification

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -151,10 +151,10 @@ const ExtendedTheme: Theme = {
     
     // Obtain configuration from: https://giscus.app/
     giscusTalk({
-      repo: 'Jackiexiao/nolebase-template',
-      repoId: 'R_kgDOL5WHsg',
+      repo: 'TangbohuDianPaoZhang/nolebase',
+      repoId: 'R_kgDOOFG_Xg',
       category: 'Announcements', // default: `General`
-      categoryId: 'DIC_kwDOL5WHss4CfTYs',
+      categoryId: 'DIC_kwDOOFG_Xs4CoGGX',
       mapping: 'url', // default: `pathname`
       inputPosition: 'top', // default: `top`
       lang: 'zh-CN', // default: `zh-CN`
